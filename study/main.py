d4e161b24e06f892f77ee295d0bbc3f541a1b4be
Yu Bin <subenmoom50@gmail.com>
2021-09-08 11:04:53 +0900

    decorator

--- study/main.py
+++ study/main.py
@@ -1,19 +1,18 @@
-class Knight:
-    def __init__(self, health, mana, armor):
-        self.health = health
-        self.mana = mana
-        self.armor = armor
+def type_check(a_type, b_type):
+    def deco(func):
+        def wrapper(a, b):
+            if ((type(a) != a_type) or (type(b) != b_type)):
+                raise RuntimeError('자료형이 올바르지 않습니다.')
+            else:
+                return func(a,b)
+        return wrapper
+    return deco
 
-    def status(self):
-        print("health : " + str(self.health))
-        print("mana : " + str(self.mana))
-        print("armor : " + str(self.armor))
+# 처음에 if 조건을 둘이 같으면 함수 출력으로 했는데 안되서 반대로 했는데 됨;;
 
-    def slash(self):
-        print("베기?")
-
-
-x = Knight(health=542.4, mana=210.3, armor=38)
-x.slash()
-x.status()
-# 여기 노래방 아닌데
+@type_check(int, int)
+def add(a, b):
+    return a + b
+ 
+print(add(10, 20))
+print(add('hello', 'world'))
