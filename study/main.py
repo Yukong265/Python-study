0308e05457d72f500ca0ced9993cbad9c40578b2
Yu Bin <subenmoom50@gmail.com>
2021-09-11 02:18:59 +0900

    파일 읽기, 코루틴

--- study/main.py
+++ study/main.py
@@ -1,18 +1,38 @@
-def type_check(a_type, b_type):
-    def deco(func):
-        def wrapper(a, b):
-            if ((type(a) != a_type) or (type(b) != b_type)):
-                raise RuntimeError('자료형이 올바르지 않습니다.')
-            else:
-                return func(a,b)
-        return wrapper
-    return deco
-
-# 처음에 if 조건을 둘이 같으면 함수 출력으로 했는데 안되서 반대로 했는데 됨;;
-
-@type_check(int, int)
-def add(a, b):
-    return a + b
+# class MultipleIterator:
+#     def __init__(self, stop, multiple):
+#         self.stop = stop
+#         self.multiple = multiple
+#         self.current = 0         
  
-print(add(10, 20))
-print(add('hello', 'world'))
+#     def __iter__(self):
+#         return self
+ 
+#     def __next__(self):
+#         self.current += 1
+#         if (self.current * self.multiple) < self.stop:
+#             return self.current * self.multiple
+#         raise StopIteration
+                                                     
+ 
+# for i in MultipleIterator(20, 3):
+#     print(i, end=' ')
+ 
+# print()
+# for i in MultipleIterator(30, 5):
+#     print(i, end=' ')
+
+                                 
+def find(words):
+    result = False # 기본 값을 false로 설정
+    while True: 
+        line = (yield result) # 문자열을 받아옴
+        result = words in line # line안에 words가 있는지 확인 
+                                 
+f = find('Python') # 코루틴 실행
+a = next(f)
+print(a)
+print(f.send('Hello, Python!')) # send로 문자열 전송
+print(f.send('Hello, world!')) 
+print(f.send('Python Script'))
+ 
+f.close() # 코루틴 중단
