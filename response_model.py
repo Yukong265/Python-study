697affec2526e263e1eaae48be0b4103cd04aaca
Yu Bin <subenmoom50@gmail.com>
2021-10-05 18:11:55 +0900

    response_model

--- response_model.py
+++ response_model.py
@@ -1,23 +1,43 @@
 from typing import Optional
 
 from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class UserIn(BaseModel):
-    username: str
-    password: str
-    email: EmailStr
-    full_name: Optional[str] = None
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: float = 10.5
 
-class UserOut(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: Optional[str] = None
 
-# Don't do this in production!
-@app.post("/user/", response_model=UserOut)
-async def create_user(user: UserIn):
-    return user
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The Bar fighters", "price": 62, "tax": 20.2},
+    "baz": {
+        "name": "Baz",
+        "description": "There goes my baz",
+        "price": 50.2,
+        "tax": 10.5,
+    },
+}
+
+
+@app.get(
+    "/items/{item_id}/name",
+    response_model=Item,
+
+    response_model_include={"name", "description"},
+
+)
+async def read_item_name(item_id: str):
+    return items[item_id]
+
+
+
+@app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"})
+
+async def read_item_public_data(item_id: str):
+    return items[item_id]
