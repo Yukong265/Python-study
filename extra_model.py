54a02ca8ca9d6f7410bfb5448e9a8eb0e4a2238d
Yu Bin <subenmoom50@gmail.com>
2021-10-06 19:37:43 +0900

    Reduce duplication

--- extra_model.py
+++ extra_model.py
@@ -6,24 +6,22 @@ from pydantic import BaseModel, EmailStr
 app = FastAPI()
 
 
-class UserIn(BaseModel):
+class UserBase(BaseModel):
     username: str
+    email: EmailStr
+    full_name: Optional[str] = None
+
+
+class UserIn(UserBase):
     password: str
-    email: EmailStr
-    full_name: Optional[str] = None
 
 
-class UserOut(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: Optional[str] = None
+class UserOut(UserBase):
+    pass
 
 
-class UserInDB(BaseModel):
-    username: str
+class UserInDB(UserBase):
     hashed_password: str
-    email: EmailStr
-    full_name: Optional[str] = None
 
 
 def fake_password_hasher(raw_password: str):
@@ -33,12 +31,11 @@ def fake_password_hasher(raw_password: str):
 def fake_save_user(user_in: UserIn):
     hashed_password = fake_password_hasher(user_in.password)
     user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
-    print(user_in.dict())
-    print(user_in_db)
     return user_in_db
 
 
 @app.post("/user/", response_model=UserOut)
 async def create_user(user_in: UserIn):
     user_saved = fake_save_user(user_in)
+    print(UserOut)
     return user_saved
