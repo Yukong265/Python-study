a133e7a0a18155b7975ff9b407bd3421ea1ed899
Yu Bin <subenmoom50@gmail.com>
2021-10-14 20:30:02 +0900

    docstring

--- Path_operation_configuration.py
+++ Path_operation_configuration.py
@@ -1,25 +1,33 @@
 from typing import Optional, Set
 
-from fastapi import FastAPI, responses, status
+from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class Item(BaseModel):
-    name:str
+    name: str
     description: Optional[str] = None
     price: float
     tax: Optional[float] = None
-    tag: Set[str] = []
+    tags: Set[str] = []
 
-@app.post("/items/", response_model=Item, status_code=status.HTTP_201_CREATED)
+
+@app.post(
+    "/items/",
+    response_model=Item,
+    summary="Create an item",
+    response_description="The created item",
+)
 async def create_item(item: Item):
+    """
+    Create an item with all the information:
+
+    - **name**: each item must have a name
+    - **description**: a long description
+    - **price**: required
+    - **tax**: if the item doesn't have tax, you can omit this
+    - **tags**: a set of unique tag strings for this item
+    """
     return item
-
-@app.get("/items/", tags=["items"])
-async def read_items():
-    return [{"name": "Foo", "price": 42}]
-
-@app.get("/users/", tags=["users"])
-async def read_users():
-    return [{"username": "johndoe"}]
