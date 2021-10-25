2839539a9939740fbcba542ab13e5ae9ed4bdf0b
Yu Bin <subenmoom50@gmail.com>
2021-10-25 17:19:55 +0900

    body_update and Dependencies first

--- body_update.py
+++ body_update.py
@@ -32,3 +32,14 @@ async def update_item(item_id: str, item: Item):
     update_item_encoded = jsonable_encoder(item)
     items[item_id] = update_item_encoded
     return update_item_encoded
+
+@app.patch("/items/{item_id}", response_model=Item)
+async def update_item(item_id: str, item: Item):
+    stored_item_data = items[item_id]
+    stored_item_model = Item(**stored_item_data)
+    update_data = item.dict(exclude_unset=True)
+    print(update_data)
+    updated_item = stored_item_model.copy(update=update_data)
+    print(updated_item)
+    items[item_id] = jsonable_encoder(updated_item)
+    return updated_item
